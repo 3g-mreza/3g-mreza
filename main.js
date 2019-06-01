227160a71be98b5187dbf6bae8369d43cd52de84
Nataša Vuković <44097248+natasa1707@users.noreply.github.com>
2019-06-01 14:32:41 +0200

    gifovi

--- main.js
+++ main.js
@@ -35,19 +35,18 @@ $(function(){
     });
 });
 const ourTeam = [
-    {name:"Aleksandra Stanojević", github:"https://github.com/aleksandrastanojevic", imgSource:"./gif/saska.gif"},
-    {name:"Vladimir Simonović", github:"https://github.com/SimaoKovin", imgSource:"./gif/vladimir.gif"},
-    {name:"Danijela Reljić", github:"https://github.com/danijelareljic", imgSource:"./gif/danijela.gif" },
-    {name:"Ivan Stojković", github:"https://github.com/iks94", imgSource:"./gif/image2.gif"},
-    {name:"Irena Šestak", github:"https://github.com/9999starlight", imgSource:"./gif/irena.gif"},
-    {name:"Marija Šobić", github:"https://github.com/marijasobic", imgSource:"./gif/marija.gif"},
-    {name:"Martina Trajković", github:"https://github.com/MartinaTrajkovic", imgSource:"./gif/image.gif"},
-    {name:"Nataša Vuković", github:"https://github.com/natasa1707", imgSource:"././gif/natasa.gif"},
-    {name:"Nenad Vukašinović", github:"https://github.com/nenadvuk", imgSource:"./gif/nenad.gif"},
-    {name:"Rada Popović", github:"https://github.com/radapopovic", imgSource:"./gif/rada.gif"},
-    {name:"Slavko Vranešević", github:"https://github.com/SlavkoVran", imgSource:"./gif/slavko.gif"},
-    {name:"Tadija Selaković", github:"https://github.com/tadija84", imgSource:"./gif/tadija.gif"}
-    
+    {name:"Aleksandra Stanojević", github:"https://github.com/aleksandrastanojevic", imgSource:"./gif/saska"},
+    {name:"Vladimir Simonović", github:"https://github.com/SimaoKovin", imgSource:"./gif/vladimir"},
+    {name:"Danijela Reljić", github:"https://github.com/danijelareljic", imgSource:"./gif/danijela" },
+    {name:"Ivan Stojković", github:"https://github.com/iks94", imgSource:"./gif/bezslike"},
+    {name:"Irena Šestak", github:"https://github.com/9999starlight", imgSource:"./gif/irena"},
+    {name:"Marija Šobić", github:"https://github.com/marijasobic", imgSource:"./gif/marija"},
+    {name:"Martina Trajković", github:"https://github.com/MartinaTrajkovic", imgSource:"./gif/bezslike"},
+    {name:"Nataša Vuković", github:"https://github.com/natasa1707", imgSource:"././gif/natasa"},
+    {name:"Nenad Vukašinović", github:"https://github.com/nenadvuk", imgSource:"./gif/nenad"},
+    {name:"Rada Popović", github:"https://github.com/radapopovic", imgSource:"./gif/rada"},
+    {name:"Slavko Vranešević", github:"https://github.com/SlavkoVran", imgSource:"./gif/slavko"},
+    {name:"Tadija Selaković", github:"https://github.com/tadija84", imgSource:"./gif/tadija"}
 ]
 
 function cardCreator () {
@@ -59,9 +58,18 @@ function cardCreator () {
         <a href="${ourTeam[i].github}" class="git-hub">github</a>
       </div>` 
       let cardImage = $('.card-image');
-      cardImage[i].style.backgroundImage = `url(${ourTeam[i].imgSource})`;
+      cardImage[i].style.backgroundImage = `url(${ourTeam[i].imgSource}.png)`;
+      
     }
-    
+    $(".card-image").each(function(i){
+        $(this).mouseover(function(){
+            $(this).css('background-image', `url(${ourTeam[i].imgSource}.gif)`);
+        });
+        $(this).mouseout(function(){
+            $(this).css('background-image', `url(${ourTeam[i].imgSource}.png)`);
+          });
+        });  
 }
+
 
 $('.button').on('click',cardCreator());
